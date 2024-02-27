ebdd7e1b0fdab7b37645a77f4fdc906e95307695
Heeseung Ha <ssigu69@gmail.com>
2024-02-27 15:57:47 +0900

    feature: 블로그 제목 클릭 시 홈화면 이동

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center p-2">
-      <h1 className="text-3xl font-bold">My Devlog</h1>
+      <h1 className="text-3xl font-bold">
+        <Link href="/">My Devlog</Link>
+      </h1>
       <ul className="flex items-center space-x-3">
         <Link href="/">
           <li>Home</li>
