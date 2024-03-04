2fff38da90e606ba9091ef5aca3874be400e3eb1
Heeseung Ha <ssigu69@gmail.com>
2024-03-04 18:23:24 +0900

    design: contact 페이지 CSS 추가

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,32 @@
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
+import { SiVelog } from "react-icons/si";
+
 export default function ContactPage() {
-  return <div>Contact</div>;
+  return (
+    <section>
+      <div className="text-center">
+        <h1 className="text-3xl font-bold">Contact Me</h1>
+        <p className="my-2">ssigu69@gmail.com</p>
+        <div className="flex items-center justify-center space-x-5">
+          <Link href="https://github.com/hheeseung">
+            <FaGithub className="text-4xl" />
+          </Link>
+          <Link href="https://velog.io/@hheeseung/posts">
+            <SiVelog className="text-4xl" />
+          </Link>
+        </div>
+        <p className="my-10 text-3xl font-bold">Or Send me Email</p>
+      </div>
+      <form className="bg-slate-700 flex flex-col w-1/3 mx-auto p-5 space-y-2 mb-3 rounded-xl">
+        <label className="text-white font-semibold">Your Email</label>
+        <input type="email" className="focus:outline-none p-2" />
+        <label className="text-white font-semibold">Subject</label>
+        <input type="text" className="focus:outline-none p-2" />
+        <label className="text-white font-semibold">Message</label>
+        <textarea rows={10} className="focus:outline-none p-2" />
+        <button className="mt-3 bg-yellow-300 font-semibold p-2">Submit</button>
+      </form>
+    </section>
+  );
 }
