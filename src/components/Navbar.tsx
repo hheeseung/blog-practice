c07e951ef27ac79b7536ba1c57d6e1972d98a525
Heeseung Ha <ssigu69@gmail.com>
2024-03-04 17:30:12 +0900

    design: 현재 경로에 따른 navbar의 underline 추가

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,11 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+  console.log(pathname);
+
   return (
     <nav className="flex justify-between items-center p-2">
       <h1 className="text-3xl font-bold">
@@ -8,16 +13,36 @@ export default function Navbar() {
       </h1>
       <ul className="flex items-center space-x-3">
         <Link href="/">
-          <li>Home</li>
+          <li className={`${pathname === "/" && "border-b-2 border-sky-500"}`}>
+            Home
+          </li>
         </Link>
         <Link href="/about">
-          <li>About</li>
+          <li
+            className={`${
+              pathname === "/about" && "border-b-2 border-sky-500"
+            }`}
+          >
+            About
+          </li>
         </Link>
         <Link href="/posts">
-          <li>Posts</li>
+          <li
+            className={`${
+              pathname === "/posts" && "border-b-2 border-sky-500"
+            }`}
+          >
+            Posts
+          </li>
         </Link>
         <Link href="/contact">
-          <li>Contact</li>
+          <li
+            className={`${
+              pathname === "/contact" && "border-b-2 border-sky-500"
+            }`}
+          >
+            Contact
+          </li>
         </Link>
       </ul>
     </nav>
