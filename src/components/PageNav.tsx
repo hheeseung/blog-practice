5bf21d167df416e5118e3e44c08a9b3a0d1b4431
Heeseung Ha <ssigu69@gmail.com>
2024-03-05 16:54:36 +0900

    refactor: 포스팅 하단 이전, 다음 목록 조건문 변경

--- src/components/PageNav.tsx
+++ src/components/PageNav.tsx
@@ -14,13 +14,13 @@ export default async function PageNav({
         prev === null || next === null ? "" : "grid-cols-2"
       } place-items-center text-center h-48 bg-gradient-to-r from-slate-100 to-slate-400 relative rounded-bl-xl rounded-br-xl`}
     >
-      {prev !== null && (
+      {prev && (
         <Link href={`/posts/${prev.path}`}>
           <FaArrowLeft className="absolute top-1/2 left-20 transform -translate-x-1/2 -translate-y-1/2 text-4xl" />
           <PageNavItem title={prev.title} description={prev.description} />
         </Link>
       )}
-      {next !== null && (
+      {next && (
         <Link href={`/posts/${next.path}`}>
           <FaArrowRight className="absolute top-1/2 right-10  transform -translate-x-1/2 -translate-y-1/2 text-4xl" />
           <PageNavItem title={next.title} description={next.description} />
